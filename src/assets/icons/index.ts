c37389cdf6a8419011de13396f0d4c8aab7a8612
finma <firmanmaulana2403@gmail.com>
2023-02-07 16:49:02 +0700

    add icons for bottom navigator

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,15 @@
+import IconHome from './home.svg';
+import IconHomeActive from './home-active.svg';
+import IconProfile from './profile.svg';
+import IconProfileActive from './profile-active.svg';
+import IconJersey from './jersey.svg';
+import IconJerseyActive from './jersey-active.svg';
+
+export {
+  IconHome,
+  IconHomeActive,
+  IconProfile,
+  IconProfileActive,
+  IconJersey,
+  IconJerseyActive,
+};
