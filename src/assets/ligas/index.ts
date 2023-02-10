1dbb9f7d15e706f7eb0d6ab865adfa41fc5d9361
finma <firmanmaulana2403@gmail.com>
2023-02-10 21:07:41 +0700

    add logo liga

---
+++ src/assets/ligas/index.ts
@@ -0,0 +1,6 @@
+import BundesLiga from './bundesliga.png';
+import LaLiga from './laliga.png';
+import PremierLeague from './premierleague.png';
+import SerieA from './seriea.png';
+
+export {BundesLiga, LaLiga, PremierLeague, SerieA};
