14d270ed448b7e0166e9e1d916865a1333a8a55d
finma <firmanmaulana2403@gmail.com>
2023-02-10 21:43:36 +0700

    fix import export component

---
+++ src/components/big/index.ts
@@ -0,0 +1,7 @@
+import BottomNavigator from './BottomNavigator';
+import TabItem from './TabItem';
+import Header from './Header';
+import BannerSlider from './BannerSlider';
+import ListLiga from './ListLiga';
+
+export {BannerSlider, BottomNavigator, TabItem, Header, ListLiga};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,2 @@
-import BottomNavigator from './big/BottomNavigator';
-import TabItem from './big/TabItem';
-import Header from './big/Header';
-import BannerSlider from './big/BannerSlider';
-import ListLiga from './big/ListLiga';
-
-export {BannerSlider, BottomNavigator, TabItem, Header, ListLiga};
+export * from './big';
+export * from './small';
