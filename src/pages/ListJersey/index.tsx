4a9af52ea7b1af4f763f8b337ad241e3e4bcf51e
finma <firmanmaulana2403@gmail.com>
2023-02-11 09:43:13 +0700

    create list jersey page

--- src/pages/ListJersey/index.tsx
+++ src/pages/ListJersey/index.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
+
+import {Gap, Header, ListJerseys, ListLiga} from '../../components';
+import {dummyJerseys, dummyLigas} from '../../data';
 
 const ListJersey = () => {
+  const [ligas] = useState(dummyLigas);
+  const [jerseys] = useState(dummyJerseys);
+
   return (
     <View style={styles.container}>
-      <Text>ListJersey Screen</Text>
+      <Header />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        style={styles.scrollContainer}>
+        <View style={styles.chooseLiga}>
+          <ListLiga ligas={ligas} />
+        </View>
+
+        <View style={styles.chooseJersey}>
+          <Text style={styles.label}>
+            Pilih <Text style={styles.boldLabel}>Jersey</Text> Yang Anda
+            Inginkan
+          </Text>
+          <ListJerseys jerseys={jerseys} />
+        </View>
+
+        <Gap height={80} />
+      </ScrollView>
     </View>
   );
 };
@@ -12,5 +35,23 @@ const ListJersey = () => {
 export default ListJersey;
 
 const styles = StyleSheet.create({
-  container: {flex: 1, alignItems: 'center', justifyContent: 'center'},
+  container: {flex: 1},
+  scrollContainer: {
+    marginTop: -30,
+  },
+  chooseLiga: {
+    marginHorizontal: 30,
+  },
+  chooseJersey: {
+    marginHorizontal: 30,
+    marginTop: 20,
+  },
+  label: {
+    fontSize: 18,
+    fontFamily: 'PublicSans-Regular',
+  },
+  boldLabel: {
+    fontSize: 18,
+    fontFamily: 'PublicSans-Bold',
+  },
 });
