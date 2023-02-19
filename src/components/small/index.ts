479ab1fdc215a3dd5ab1bdebe3e5166a9d49cdc5
finma <firmanmaulana2403@gmail.com>
2023-02-19 11:08:11 +0700

    add input & select component to index

--- src/components/small/index.ts
+++ src/components/small/index.ts
@@ -3,5 +3,7 @@ import Gap from './Gap';
 import CardLiga from './CardLiga';
 import CardJersey from './CardJersey';
 import CardMenu from './CardMenu';
+import Input from './Input';
+import Select from './Select';
 
-export {Button, Gap, CardLiga, CardJersey, CardMenu};
+export {Button, Gap, CardLiga, CardJersey, CardMenu, Input, Select};
