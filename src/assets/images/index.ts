3f635cbaeb06ebf9acd1d0193387f82f1ab9e402
finma <firmanmaulana2403@gmail.com>
2023-02-21 10:18:19 +0700

    change illustration format svg to tsx

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,5 +1,5 @@
 import Logo from './logo.svg';
-import Illustration from './illustration.svg';
+import Illustration from './illustration';
 import Slider1 from './slider1.png';
 import Slider2 from './slider2.png';
 import FotoProfile from './profile.png';
