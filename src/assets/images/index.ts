0f091cc809694d2cc94e33f766c7ecfc9b5ee53e
finma <firmanmaulana2403@gmail.com>
2023-02-23 21:40:59 +0700

    add illustration register1 & register2

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -3,5 +3,15 @@ import Illustration from './illustration';
 import Slider1 from './slider1.png';
 import Slider2 from './slider2.png';
 import FotoProfile from './profile.png';
+import IllustrationRegister1 from './register1';
+import IllustrationRegister2 from './register2';
 
-export {Logo, Illustration, Slider1, Slider2, FotoProfile};
+export {
+  Logo,
+  Illustration,
+  Slider1,
+  Slider2,
+  FotoProfile,
+  IllustrationRegister1,
+  IllustrationRegister2,
+};
