c24fd126e3046239d78f549747ec61d5d39d1c44
finma <firmanmaulana2403@gmail.com>
2023-02-25 16:44:48 +0700

    add api rajaongkir

--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -1,2 +1,7 @@
 export const heightMobileUI = 896;
 export const widthMobileUI = 414;
+
+export const API_KEY = '837ecc882657f1676fe114ba3fed1b7d';
+export const API_URL_RAJAONGKIR = 'https://api.rajaongkir.com/starter';
+export const API_HEADER_RAJAONGKIR = {key: API_KEY};
+export const API_TIMEOUT = 120000;
