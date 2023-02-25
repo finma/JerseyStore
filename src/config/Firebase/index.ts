8d137ca42b61198d0b45dccd1d0878ea7f997de5
finma <firmanmaulana2403@gmail.com>
2023-02-25 21:45:21 +0700

    create config firebase

---
+++ src/config/Firebase/index.ts
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyBm0kTSkVmKM4SIdyryu0GSY-3zamjPzK4',
+  authDomain: 'jerseystore-270bc.firebaseapp.com',
+  projectId: 'jerseystore-270bc',
+  storageBucket: 'jerseystore-270bc.appspot.com',
+  messagingSenderId: '613767990521',
+  appId: '1:613767990521:web:a4100f39e6f3fbf3c1e628',
+  measurementId: 'G-44SPK0DJXS',
+});
+
+const Firebase = firebase;
+
+export default Firebase;
