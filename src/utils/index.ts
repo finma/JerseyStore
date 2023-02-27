bacf0085ecdde28635ddc874767fdec8b6ac2983
finma <firmanmaulana2403@gmail.com>
2023-02-27 22:13:28 +0700

    add async storage

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,4 @@ export * from './colors';
 export * from './fonts';
 export * from './utils';
 export * from './constants';
+export * from './localStorage';
---
+++ src/utils/localStorage/index.ts
@@ -0,0 +1,31 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+// Store data in AsyncStorage
+export const storeData = async (key: string, value: any) => {
+  try {
+    await AsyncStorage.setItem(key, JSON.stringify(value));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Get data from AsyncStorage
+export const getData = async (key: string) => {
+  try {
+    const value = await AsyncStorage.getItem(key);
+    if (value !== null) {
+      return value;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Remove data from AsyncStorage
+export const removeData = async (key: string) => {
+  try {
+    await AsyncStorage.removeItem(key);
+  } catch (error) {
+    console.log(error);
+  }
+};
